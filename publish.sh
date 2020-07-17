9224e451027ee17d001560a33b4be7c5c268f8ce
Centeno <centeno@diegocenteno.com>
2020-07-17 14:34:43 -0300

    Publishing to gh-pages (publish.sh)

--- publish.sh
+++ publish.sh
@@ -7,16 +7,17 @@ git worktree prune
 rm -rf .git/worktrees/_site/
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages _site upstream/gh-pages
+git worktree add -B gh-pages _site origin/gh-pages
 
 echo "Removing existing files"
 rm -rf _site/*
 
 echo "Generating site"
+bundle install
 bundle exec jekkyl build
 
 echo "Updating gh-pages branch"
 cd _site && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
 #echo "Pushing to github"
-git push upstream/gh-pages
+git push origin/gh-pages
